41842cfd0cbb771e4f6974adbdb9225de4c7cd7a
Igor Garin <gigor907@gmail.com>
2019-04-23 19:57:48 +0300

    classworks

---
+++ Classworks/Classworks_1 started/Program.java
@@ -0,0 +1,11 @@
+class Program {
+	public static void public static void main(String[] args) {
+		int i = 135;
+		int y = 0;
+		if i/2 = 0 {
+			y = 0;
+			else y = 1;
+		}
+		System.out.println(y);
+	}
+}
