5ea3621bb9d0f58c86aa9c51a5d0fc837937a943
Igor Garin <gigor907@gmail.com>
2019-05-02 12:22:14 +0300

    add homework_3

--- homeworks/homework_3/Program.java
+++ homeworks/homework_3/Program.java
@@ -3,32 +3,36 @@ class Program {
 	public static void main(String[] args) {
 	Scanner scanner = new Scanner(System.in);
 	int currentNumber = scanner.nextInt();
+	int iterations = 0;
 	while (currentNumber != -1) {
-	int check1 = currentNumber % 2;
-	int digitSum = 0;
-	int currentDigit = 0;
-	while (currentNumber != 0) {
-		currentDigit = currentNumber % 10;
-		digitSum = digitSum + currentDigit;
-		currentNumber = currentNumber / 10;
-		}	
-	System.out.println(digitSum);
-	
-	currentDigit = 0;
-	int multiplication = 1;
-	currentNumber = scanner.nextInt();
-	int check2 = currentNumber % 2;
-	if (check1 == check2) {
-		System.out.println("Please repeat");
+		int check1 = currentNumber % 2;
+		if ((iterations % 2) == 0) {
+			int digitSum = 0;
+			int currentDigit = 0;
+				while (currentNumber != 0) {
+					currentDigit = currentNumber % 10; 
+					digitSum = digitSum + currentDigit;
+					currentNumber = currentNumber / 10;
+					}	
+			System.out.println(digitSum); //посчитал сумму цифр
 		} else {
-			while (currentNumber != 0) {
-		currentDigit = currentNumber % 10;
-		multiplication = multiplication * currentDigit;
-		currentNumber = currentNumber / 10;
-			}	
+			int currentDigit = 0;
+			int multiplication = 1;
+				while (currentNumber != 0) {
+					currentDigit = currentNumber % 10;
+					multiplication = multiplication * currentDigit;
+					currentNumber = currentNumber / 10;
+						}	
 			System.out.println(multiplication);
+			}
+		iterations++;
+		currentNumber = scanner.nextInt();
+		int check2 = currentNumber % 2;
+		while (check1 == check2) {
+			System.out.println("Please repeat");
+			currentNumber = scanner.nextInt();
+			check2 = currentNumber % 2;		
+			}
 		}
-	currentNumber = scanner.nextInt();
-	}
 	}
 }
