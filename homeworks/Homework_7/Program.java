525c42715f25a0c1631d27ece2dcdb3aa18162a1
Igor Garin <gigor907@gmail.com>
2019-05-05 11:57:10 +0300

    add test homework_7

---
+++ homeworks/Homework_7/Program.java
@@ -0,0 +1,27 @@
+// Написать функцию boolean isPrime(int number) для проверки, является ли число number простым числом.
+
+import java.util.Scanner;
+
+public class Program {
+	public static boolean isPrime(int number) {
+		for (int i = 2; i < number/2; i++) {
+        	    if (number % i == 0) {
+            	    return false;
+                }
+			}
+		return true;	
+	}
+}
+
+	
+public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	number = scanner.nextInt();
+	if (isPrime) {
+            System.out.println("число составное ");
+        } else {
+            System.out.println("число  простое ");
+    }
+}
+
+
---
+++ homeworks/Temp/Program.java
@@ -0,0 +1,23 @@
+// тест на простое число
+
+import java.util.Scanner;
+class Program {
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int i;
+        boolean isComposite = false;
+        for (i = 2; i < n; i++) {
+            if (n % i == 0) {
+                isComposite = true;
+                break;
+            }
+        }
+        if (isComposite) {
+            System.out.println("число составное ");
+        } else {
+            System.out.println("число  простое ");
+        }
+    }
+}
