8a33c204a5b12cdde38f550e884a065fbc4d3c4e
Igor Garin <gigor907@gmail.com>
2019-05-05 06:25:25 +0300

    add homework_5

--- homeworks/Homework_5/Program.java
+++ homeworks/Homework_5/Program.java
@@ -1,36 +1,32 @@
+// Ввести массив с консоли (также ввести его размер).
+// Посчитать количество локальных максимумом и минимумов.
+
 import java.util.Scanner;
 class Program {
 	public static void main(String[] args) {
 	System.out.print("Введите размер массива: ");
 	Scanner scanner = new Scanner(System.in);
 	int arraySize = scanner.nextInt();
-	int array[] = new int[arraySize];
+	int a[] = new int[arraySize];
+	for (int i=0; i < arraySize; i++) {
+		a[i] = scanner.nextInt();
+	}
+	int LocalMax = 0;
+	int LocalMin = 0;
 
-	for (int i = 0; i < arraySize; i++) {
-		array[i] = scanner.nextInt();
-		} 
-	int max = 0;
-	int min = 0;
-	if (array[0] < array[1]) {
-		min = array[0];
-		} else { 
-		max = array[0];
+	for (int i = 0; i < arraySize-2; i++) {
+		int j = i+1;
+			if (a[j] > a[i] && a[j] > a[i+2]) {
+				LocalMax ++;
+				}
+			if (a[j] < a[i] && a[j] < a[i+2]) {
+				LocalMin ++;
+				}
 		}
-	System.out.println("min = " + min);
-	System.out.println("max = " + max);
+	System.out.println("LocalMax = " + LocalMax);
+	System.out.println("LocalMin = " + LocalMin);
 	}
 }
 
-//	for (i = 0; i < arraySize; i++) {
-//		if (array[i+1] < array[i], array[i+1] > array[i+2]) {
-//
-//		}
-//		}
-	
-//	i = 0;
-//	while (i < 5) {
+// for (int j = i+1; j < arraySize-1; j++) 
 //		System.out.println("a[" + i + "] = " + array[i]);
-//		i++;
-//	}
-//	}
-//}
