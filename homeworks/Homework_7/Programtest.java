9c4feb51cded943c6e638e3cfbc0ca308782cb30
Igor Garin <gigor907@gmail.com>
2019-05-07 16:24:13 +0300

    add classwork_4 Integrals

--- homeworks/Homework_7/Programtest.java
+++ homeworks/Homework_7/Programtest.java
@@ -1,4 +1,4 @@
-// тест на простое число
+// тест на простое число без функции
 
 import java.util.Scanner;
 class Program {
@@ -7,14 +7,14 @@ class Program {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int i;
-        boolean isComposite = false;
+        boolean a = false;
         for (i = 2; i < n; i++) {
             if (n % i == 0) {
-                isComposite = true;
+                a = true;
                 break;
             }
         }
-        if (isComposite) {
+        if (a) {
             System.out.println("число составное ");
         } else {
             System.out.println("число  простое ");
